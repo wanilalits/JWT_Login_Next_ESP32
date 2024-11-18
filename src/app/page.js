130800e13a31d30d0c28cae458256e649fa8b633
wanilalits <wanilalits@gmail.com>
2024-11-18 16:15:20 +0530

    #

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import Login from "./login/page";
 //import Head from 'next/head';
-import Chat from './components/Chat';
+
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-    <h2>V1.1</h2>
+    <h2>V1.0</h2>
     <Link  href="/register"> register</Link>
-   
+
     </div>
   );
 }
