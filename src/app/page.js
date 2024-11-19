89140400c561906743745374cd90984a45807b81
wanilalits <wanilalits@gmail.com>
2024-11-19 16:44:20 +0530

    #

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,29 @@
-
+'use client'
 //import Head from 'next/head';
 import Link from "next/link";
-
 export default function Home() {
+const url=window.location.href
+  
+  console.log(window.location.href)
   return (
     <div>
     <h2>V1.1</h2>
 <br></br>
 
-   <Link  href="/register"> Register Link </Link>
+   <Link  href="/register"> Register Link  </Link>
+   <br></br>   
+   or Goto
    <br></br>
- <Link  href="/login"> Login Link </Link>
+   {url+'register'}
+   <br></br><br></br>   <br></br>
+ <Link  href="/login"> Login Link  </Link> 
+ <br></br> 
+   or Goto
+   <br></br>
+   {url+'login'}
+   <br></br>
+
+
 
     </div>
   );
