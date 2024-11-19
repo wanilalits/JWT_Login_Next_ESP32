b003bc5f4dd6838f19aa0c2121b08c6c93c0c44b
wanilalits <wanilalits@gmail.com>
2024-11-19 16:54:51 +0530

    #

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,28 @@
 //import Head from 'next/head';
 import Link from "next/link";
 export default function Home() {
-
+const url='https://lsw-kappa.vercel.app/'
   
-  console.log(window.location.href)
+ 
   return (
     <div>
-    <h2>V1.0</h2>
+    <h2>V1.1</h2>
 <br></br>
 
    <Link  href="/register"> Register Link  </Link>
    <br></br>   
-  
-   <br></br><br></br>  
+   or goto
+   <br></br>
+   {url+'register'}
+   <br></br><br></br>   <br></br>
  <Link  href="/login"> Login Link  </Link> 
- 
+ <br></br> 
+   or goto
+   <br></br>
+   {url+'login'}
+   <br></br>
+
+
 
     </div>
   );
